e3e5bef790077051650d447010574c368c666562
darmren <dem24@tpu.ru>
2024-03-03 23:53:51 +0700

    Voice handler was added

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import asyncio
 import os
 import logging
 import aiogram
+from aiogram import html
+from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types.voice import Voice
+from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"), parse_mode=ParseMode.MARKDOWN)
 dp = Dispatcher()
 
 
@@ -20,13 +24,22 @@ async def main():
 
 @dp.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer("Hello!")
+    await msg.answer(
+        f"Hello, {html.bold(html.quote(msg.from_user.full_name))}",
+        parse_mode=ParseMode.HTML
+    )
+
+
+@dp.message(F.voice)
+async def voice_handler(msg: Message):
+    await msg.answer("Voice")
 
 
 @dp.message(F.text)
 async def echo_handler(msg: Message):
     text = msg.text
     await msg.answer(text)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
